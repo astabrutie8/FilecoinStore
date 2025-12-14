1c955bf836a887ef57864247db47d683ef4ecbda
Brutie <125909656+astabrutie8@users.noreply.github.com>
2025-12-15 00:55:25 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/filecoinstore/main.go
@@ -0,0 +1,23 @@
+// cmd/filecoinstore/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "filecoinstore/internal/filecoinstore"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := filecoinstore.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
